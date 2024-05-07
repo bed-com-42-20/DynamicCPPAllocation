87c1d2f06b92c9b8b82f851a8c96240f7dc95bea
amos chaluso <bed-com-42-20@unima.ac.mw>
2024-05-07 06:40:00 +0200

    Made changes to files in LAB6 directory

--- dynamic2.cpp
+++ dynamic2.cpp
@@ -1,44 +1,50 @@
-#include <iostream>
+ #include <iostream>
 
 using namespace std;
 
 namespace mynamespace {
-void inputMatrix(double** matrix, int rows, int cols) {
-for (int i = 0; i < rows; ++i)
-for (int j = 0; j < cols; ++j)
-cin >> matrix[i][j];
+    void inputMatrix(double** matrix, int rows, int columns) {
+        for (int i = 0; i < rows; ++i)
+            for (int j = 0; j < columns; ++j)
+                cin >> matrix[i][j];
     }
-void displayMatrix(const double** matrix, int rows, int cols) {
-for (int i = 0; i < rows; ++i) {
-for (int j = 0; j < cols; ++j)
-cout << matrix[i][j] << "\t";
-cout << endl;
+
+    void displayMatrix(const double** matrix, int rows, int columns) {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j)
+                cout << matrix[i][j] << "\t";
+            cout << endl;
         }
     }
-int main() {
-int rows, cols;
-do {
-cout << "Enter number of rows (<=3): ";
-cin >> rows;
-cout << "Enter number of columns (<=3): ";
-cin >> cols;
 
-if (rows > 3 || cols > 3)
-cout << "Invalid input! Both rows and columns should be <= 3." << endl;
-} 
-while (rows > 3 || cols > 3);
-double** array = new double*[rows];
-for (int i = 0; i < rows; ++i)
-array[i] = new double[cols];
-inputMatrix(array, rows, cols);
-displayMatrix(const_cast<const double**>(array), rows, cols);
-for (int i = 0; i < rows; ++i)
-delete[] array[i];
-delete[] array;
+    int main() {
+        int rows, columns;
+        const int MAX_COLUMNS = 3; // Define the maximum number of columns
+        do {
+            cout << "Enter number of rows (<=3): ";
+            cin >> rows;
+            cout << "Enter number of columns (<= " << MAX_COLUMNS << "): ";
+            cin >> columns;
 
-return 0;
+            if (rows > 3 || columns > MAX_COLUMNS)
+                cout << "Invalid input! Rows should be <= 3 and columns should be <= " << MAX_COLUMNS << "." << endl;
+        } while (rows > 3 || columns > MAX_COLUMNS);
+
+        double** array = new double*[rows];
+        for (int i = 0; i < rows; ++i)
+            array[i] = new double[columns];
+
+        inputMatrix(array, rows, columns);
+        displayMatrix(const_cast<const double**>(array), rows, columns);
+
+        for (int i = 0; i < rows; ++i)
+            delete[] array[i];
+        delete[] array;
+
+        return 0;
     }
 }
+
 int main() {
-return mynamespace::main();
+    return mynamespace::main();
 }
